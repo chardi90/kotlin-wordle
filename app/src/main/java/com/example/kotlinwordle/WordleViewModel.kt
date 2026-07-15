dfe14af02b3c3dda1db3c6e4836272a3f3e0bbe0
hira <228408573+hirabatul@users.noreply.github.com>
2026-07-15 17:05:32 +0100

    ViewModel completed w/o errors, passed through to all composables successfully, page redirect should be working.

--- app/src/main/java/com/example/kotlinwordle/WordleViewModel.kt
+++ app/src/main/java/com/example/kotlinwordle/WordleViewModel.kt
@@ -2,10 +2,33 @@ package com.example.kotlinwordle
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class WordleViewModel: viewModel() {
+class WordleViewModel: ViewModel() {
     var rowIndex by mutableIntStateOf(0)
         private set
+
+    var playIsClicked by mutableStateOf(false)
+        private set
+
+    var submitIsClicked by mutableStateOf(false)
+        private set
+
+    var homeIsClicked by mutableStateOf(false)
+        private set
+
+    fun playGame() {
+        playIsClicked = !playIsClicked
+    }
+
+    fun submitRow() {
+        submitIsClicked = !submitIsClicked
+    }
+
+    fun returnHome() {
+        homeIsClicked = !homeIsClicked
+        rowIndex = 0
+    }
 }
