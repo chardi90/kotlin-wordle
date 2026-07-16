5120cabc27d5ff09b9e9454005efe871fe53cef1
chardi90 <chardi90@hotmail.com>
2026-07-16 12:48:05 +0100

    created textfield method inside square

---
+++ app/src/main/java/com/example/kotlinwordle/components/Square.kt
@@ -0,0 +1,31 @@
+package com.example.kotlinwordle.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+
+@Composable
+fun Square(
+    modifier: Modifier = Modifier
+) {
+    val maxLength = 1
+    var letterInput = ""
+    Box() {
+        TextField(
+            value = letterInput,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                capitalization = KeyboardCapitalization.Characters),
+            onValueChange = { newValue ->
+                if (newValue.isEmpty() || newValue.matches(Regex("^[A-Z]+$")) || newValue.length == maxLength)
+                    letterInput = newValue
+            },
+            modifier = modifier
+        )
+    }
+}
