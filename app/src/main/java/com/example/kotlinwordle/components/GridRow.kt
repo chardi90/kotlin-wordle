6ae88e12ccb04ecc65ad371c58548fafff71c7fc
chardi90 <chardi90@hotmail.com>
2026-07-17 09:12:25 +0100

    reworded variables so error diappeared

--- app/src/main/java/com/example/kotlinwordle/components/GridRow.kt
+++ app/src/main/java/com/example/kotlinwordle/components/GridRow.kt
@@ -16,51 +16,51 @@ fun GridRow(
     modifier: Modifier = Modifier
 ) {
     val wordToday = stringResource(id = viewModel.word.word)
-    var background_1 = Color.DarkGray
-    var background_2 = Color.DarkGray
-    var background_3 = Color.DarkGray
-    var background_4 = Color.DarkGray
-    var background_5 = Color.DarkGray
+    var backgroundOne = Color.DarkGray
+    var backgroundTwo = Color.DarkGray
+    var backgroundThree = Color.DarkGray
+    var backgroundFour = Color.DarkGray
+    var backgroundFive = Color.DarkGray
 
     if (viewModel.submitIsClicked) {
         if (viewModel.letterInput_1 == wordToday[0].toString()) {
-            background_1 = Color.Green
+            backgroundOne = Color.Green
         } else if (wordToday.contains(viewModel.letterInput_1)) {
-            background_1 = Color.Yellow
+            backgroundOne = Color.Yellow
         } else {
-            background_1 = Color.DarkGray
+            backgroundOne = Color.DarkGray
         }
 
         if (viewModel.letterInput_2 == wordToday[1].toString()) {
-            background_2 = Color.Green
+            backgroundTwo = Color.Green
         } else if (wordToday.contains(viewModel.letterInput_2)) {
-            background_2 = Color.Yellow
+            backgroundTwo = Color.Yellow
         } else {
-            background_2 = Color.DarkGray
+            backgroundTwo = Color.DarkGray
         }
 
         if (viewModel.letterInput_3 == wordToday[2].toString()) {
-            background_3 = Color.Green
+            backgroundThree = Color.Green
         } else if (wordToday.contains(viewModel.letterInput_3)) {
-            background_3 = Color.Yellow
+            backgroundThree = Color.Yellow
         } else {
-            background_3 = Color.DarkGray
+            backgroundThree = Color.DarkGray
         }
 
         if (viewModel.letterInput_4 == wordToday[3].toString()) {
-            background_4 = Color.Green
+            backgroundFour = Color.Green
         } else if (wordToday.contains(viewModel.letterInput_4)) {
-            background_4 = Color.Yellow
+            backgroundFour = Color.Yellow
         } else {
-            background_4 = Color.DarkGray
+            backgroundFour = Color.DarkGray
         }
 
         if (viewModel.letterInput_5 == wordToday[4].toString()) {
-            background_5 = Color.Green
+            backgroundFive = Color.Green
         } else if (wordToday.contains(viewModel.letterInput_5)) {
-            background_5 = Color.Yellow
+            backgroundFive = Color.Yellow
         } else {
-            background_5 = Color.DarkGray
+            backgroundFive = Color.DarkGray
         }
     }
 
@@ -72,27 +72,27 @@ fun GridRow(
         Square(
             value = viewModel.letterInput_1,
             onInputChange = { viewModel.onInputChange_1(it) },
-            modifier = Modifier.weight(1f).background(background_1)
+            modifier = Modifier.weight(1f).background(backgroundOne)
         )
         Square(
             value = viewModel.letterInput_2,
             onInputChange = { viewModel.onInputChange_2(it) },
-            modifier = Modifier.weight(1f).background(background_2)
+            modifier = Modifier.weight(1f).background(backgroundTwo)
         )
         Square(
             value = viewModel.letterInput_3,
             onInputChange = { viewModel.onInputChange_3(it) },
-            modifier = Modifier.weight(1f).background(background_3)
+            modifier = Modifier.weight(1f).background(backgroundThree)
         )
         Square(
             value = viewModel.letterInput_4,
             onInputChange = { viewModel.onInputChange_4(it) },
-            modifier = Modifier.weight(1f).background(background_4)
+            modifier = Modifier.weight(1f).background(backgroundFour)
         )
         Square(
             value = viewModel.letterInput_5,
             onInputChange = { viewModel.onInputChange_5(it) },
-            modifier = Modifier.weight(1f).background(background_5)
+            modifier = Modifier.weight(1f).background(backgroundFive)
         )
     }
 }
